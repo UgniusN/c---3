8bf9823e2028cacd86976584fbdcfbeaf5c44012
Ugnius <ugniusnaujokas@gmail.com>
2019-12-24 04:37:38 +0200

    Completed app 6

--- app 6/app 6/Program.cs
+++ app 6/app 6/Program.cs
@@ -10,6 +10,23 @@ namespace app_6
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Iveskite skaiciu A: ");
+            String a = Console.ReadLine();
+            double A = Convert.ToDouble(a);
+
+            Console.WriteLine("Iveskite skaiciu B: ");
+            String b = Console.ReadLine();
+            double B = Convert.ToDouble(b);
+
+            Console.WriteLine("Dabar skaicius A yra: {0}, o skaicius B yra {1}", A,B);
+
+            A += B;
+            B = A - B;
+            A -= B;
+
+            Console.WriteLine("Dabar skaicius A yra: {0}, o skaicius B yra {1}", A, B);
+
+            Console.ReadLine();
         }
     }
 }
