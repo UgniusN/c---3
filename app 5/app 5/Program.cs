a28971000b6a3d5665f10782b50ff24d1c18c9d3
Ugnius <ugniusnaujokas@gmail.com>
2019-12-24 04:31:46 +0200

    Completed app 5

---
+++ app 5/app 5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite krastines x ilgi: ");
+            String ilgisLaik = Console.ReadLine();
+            double ilgisDid = Convert.ToDouble(ilgisLaik);
+
+            Console.WriteLine("Iveskite krastines y ploti: ");
+            String plotisLaik = Console.ReadLine();
+            double plotisDid = Convert.ToDouble(plotisLaik);
+
+            double plotasDid = ilgisDid * plotisDid;
+            double ilgisMaz = 2;
+            double plotisMaz = 3;
+            double plotasMaz = ilgisMaz * plotisMaz;
+
+            double kiekis = Math.Round(plotasDid / plotasMaz, 0);
+
+            Console.WriteLine("Į {0} x {0} staciakampi {2} x {3} staciakampiu tilps: {4}",ilgisDid,plotisDid,ilgisMaz,plotisMaz,kiekis);
+
+            Console.Read();
+        }
+    }
+}
