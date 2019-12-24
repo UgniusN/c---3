cf7ad04bb51f99dd055064ca8a4efa200af671ea
Ugnius <ugniusnaujokas@gmail.com>
2019-12-24 05:04:48 +0200

    Completed app 8

--- app 8/app 8/Program.cs
+++ app 8/app 8/Program.cs
@@ -10,6 +10,21 @@ namespace app_8
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Iveskite staciakampio (X;Y) Koordinates: ");
+
+            String x = Console.ReadLine();
+            int X = Convert.ToInt32(x);
+
+            String y = Console.ReadLine();
+            int Y = Convert.ToInt32(y);
+
+            Console.WriteLine("Iveskite antro staciakampio (X1;Y1) Koordinates: ");
+
+            String x1 = Console.ReadLine();
+            int X1 = Convert.ToInt32(x1);
+
+            String y1 = Console.ReadLine();
+            int Y1 = Convert.ToInt32(y1);
         }
     }
 }
