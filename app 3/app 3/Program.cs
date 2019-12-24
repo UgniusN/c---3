36cb7b0484a6fa67fb485ab1b5834be1c55d67e2
Ugnius <ugniusnaujokas@gmail.com>
2019-12-24 04:08:23 +0200

    Completed app 3

--- app 3/app 3/Program.cs
+++ app 3/app 3/Program.cs
@@ -10,6 +10,25 @@ namespace app_3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Iveskite DARBUOTOJO ID: ");
+            String ivestisId = Console.ReadLine();
+            int ID = Convert.ToInt32(ivestisId);
+
+            Console.WriteLine("Iveskite DARBUOTOJO ISDIRBTAS VALANDAS");
+            String ivestisValandos = Console.ReadLine();
+            int valandos = Convert.ToInt32(ivestisValandos);
+
+            Console.WriteLine("Iveskite DARBUOTOJO VALANDINI IKAINI ( EUR / H )");
+            String ivestisIkainis = Console.ReadLine();
+            double ikainis = Convert.ToDouble(ivestisIkainis);
+
+            double alga = valandos * ikainis;
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Darbuotojo ID: {0}", ID);
+            Console.WriteLine("Darbuotjo ALGA: {0}", alga);
+            Console.ReadLine();
         }
     }
 }
