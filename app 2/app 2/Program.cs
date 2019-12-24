36185d745dca1858ae35d6fe1bd4879b0802039c
Ugnius <ugniusnaujokas@gmail.com>
2019-12-24 03:52:16 +0200

    Completed app 2

--- app 2/app 2/Program.cs
+++ app 2/app 2/Program.cs
@@ -10,6 +10,15 @@ namespace app_2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Iveskite apskritimo spinduli(r): ");
+            String r = Console.ReadLine();
+            int radius = Convert.ToInt32(r);
+            double ilgis = 2 * radius * Math.PI;
+            double plotas = Math.PI * Math.Pow(radius, 2);
+            Console.WriteLine("Apskritimo spindulys = {0};",radius);
+            Console.WriteLine("Apskritimo plotas = {0};", plotas);
+            Console.WriteLine("Apskritimo ilgis = {0};", ilgis);
+            Console.ReadLine();
         }
     }
 }
