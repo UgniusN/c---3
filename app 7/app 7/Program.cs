baeac70496e1acbd31e6cd2baa2bdfc1d68c3606
Ugnius <ugniusnaujokas@gmail.com>
2019-12-24 04:45:28 +0200

    Completed app 7

--- app 7/app 7/Program.cs
+++ app 7/app 7/Program.cs
@@ -10,6 +10,15 @@ namespace app_7
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Iveskite dvizenklį skaičių: ");
+            String dvizenklisLaik = Console.ReadLine();
+            int dvizenklis = Convert.ToInt32(dvizenklisLaik);
+            int vienetai = dvizenklis % 10;
+            int desimtys = (dvizenklis - vienetai) / 10;
+            int skaitmenuSuma = vienetai + desimtys;
+            Console.WriteLine("Dviženklio skaiciaus {0} skaitmenu suma yra {1} + {2} = {3}", dvizenklis,desimtys, vienetai, skaitmenuSuma);
+
+            Console.Read();
         }
     }
 }
